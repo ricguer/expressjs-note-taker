287635cea6f4a5730c1846008dc784266b67497b
ricguer <rj.guerra@outlook.com>
2023-07-06 14:32:36 -0400

    Add "POST" Route to Save New Note

--- server.js
+++ server.js
@@ -24,6 +24,30 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (title && text)
+    {
+        const newNote = {
+            title,
+            text
+        };
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+        res.status(201).json(response);
+    }
+    else {
+        res.status(500).json("Error in posting note");
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
